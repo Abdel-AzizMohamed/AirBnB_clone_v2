8edc6e64cc5acbc11b58fa0067418a340e353ba1
vagrant <vagrant@linux-vag20>
2023-10-05 13:02:29 +0000

    Fix

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -7,22 +7,33 @@ env.hosts = ["100.25.21.246", "54.221.183.98"]
 
 def do_deploy(archive_path):
     """generates a .tgz archive from the contents of the web_static"""
-    if not os.path.exists(archive_path):
+    if os.path.isfile(archive_path) is False:
         return False
+    file = archive_path.split("/")[-1]
+    name = file.split(".")[0]
 
-    try:
-        file = archive_path.split("/")[-1]
-        name = file.split(".")[0]
-        path = "/data/web_static/releases/"
-        put(archive_path, "/tmp/")
-        run("mkdir -p {}{}".format(path, name))
-        run("tar -xzf /tmp/{} -C {}{}/".format(file, path, name))
-        run("rm /tmp/{}".format(file))
-        run("mv {0}{1}/web_static/* {0}{1}/".format(path, name))
-        run("rm -rf {}{}/wen_static".format(path, name))
-        run("rm -rf /data/web_static/current")
-        run("ln -s {}{}/ /data/web_static/current".format(path, name))
-
-        return True
-    except:
+    if put(archive_path, "/tmp/{}".format(file)).failed is True:
         return False
+    if run("rm -rf /data/web_static/releases/{}/".
+           format(name)).failed is True:
+        return False
+    if run("mkdir -p /data/web_static/releases/{}/".
+           format(name)).failed is True:
+        return False
+    if run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".
+           format(file, name)).failed is True:
+        return False
+    if run("rm /tmp/{}".format(file)).failed is True:
+        return False
+    if run("mv /data/web_static/releases/{}/web_static/* "
+           "/data/web_static/releases/{}/".format(name, name)).failed is True:
+        return False
+    if run("rm -rf /data/web_static/releases/{}/web_static".
+           format(name)).failed is True:
+        return False
+    if run("rm -rf /data/web_static/current").failed is True:
+        return False
+    if run("ln -s /data/web_static/releases/{}/ /data/web_static/current".
+           format(name)).failed is True:
+        return False
+    return True
--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,8 +1,58 @@
 #!/usr/bin/python3
 """Fabric script that generates a .tgz archive"""
-from 1-pack_web_static import do_pack
-from 2-do_deploy_web_static import do_deploy
+import os
+from fabric.api import local, put, run, env
+from datetime import datetime
 env.hosts = ["100.25.21.246", "54.221.183.98"]
+
+
+def do_deploy(archive_path):
+    """generates a .tgz archive from the contents of the web_static"""
+    if os.path.isfile(archive_path) is False:
+        return False
+    file = archive_path.split("/")[-1]
+    name = file.split(".")[0]
+
+    if put(archive_path, "/tmp/{}".format(file)).failed is True:
+        return False
+    if run("rm -rf /data/web_static/releases/{}/".
+           format(name)).failed is True:
+        return False
+    if run("mkdir -p /data/web_static/releases/{}/".
+           format(name)).failed is True:
+        return False
+    if run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".
+           format(file, name)).failed is True:
+        return False
+    if run("rm /tmp/{}".format(file)).failed is True:
+        return False
+    if run("mv /data/web_static/releases/{}/web_static/* "
+           "/data/web_static/releases/{}/".format(name, name)).failed is True:
+        return False
+    if run("rm -rf /data/web_static/releases/{}/web_static".
+           format(name)).failed is True:
+        return False
+    if run("rm -rf /data/web_static/current").failed is True:
+        return False
+    if run("ln -s /data/web_static/releases/{}/ /data/web_static/current".
+           format(name)).failed is True:
+        return False
+    return True
+
+
+def do_pack():
+    """generates a .tgz archive from the contents of the web_static"""
+    date = datetime.now().strftime("%Y%m%d%H%M%S")
+    tgz_name = "versions/web_static_{}.tgz".format(date)
+
+    if not os.path.exists("versions"):
+        if local("mkdir versions").failed:
+            return None
+
+    if local("tar -cvzf {} web_static".format(tgz_name)).failed:
+        return None
+
+    return tgz_file
 
 
 def deploy():
