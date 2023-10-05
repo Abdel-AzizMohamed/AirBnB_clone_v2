e3f51c549b067a6c3dfd4bd2dc4f78dbec60d899
vagrant <vagrant@linux-vag20>
2023-10-05 11:49:37 +0000

    Fix task 2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -2,31 +2,28 @@
 """Fabric script that generates a .tgz archive"""
 import os
 from fabric.api import local
-
-
 env.hosts = ["100.25.21.246", "54.221.183.98"]
+
 
 def do_deploy(archive_path):
     """generates a .tgz archive from the contents of the web_static"""
-    file = archive_path.split("/")[-1]
-    name = file.split(".")[0]
-
     if not os.path.exists(archive_path):
         return False
 
-    if put(archive_path, "/tmp/{}".format(file)).failed:
-        return False
+    file = archive_path.split("/")[-1]
+    name = file.split(".")[0]
+    path = "/data/web_static/releases/"
 
-    if run("tar -xfz /tmp/{} -C /data/web_static/releases/{}".format(file, name)).failed:
-        return False
+    try:
+        put(archive_path, "/tmp/")
+        run("mkdir -p {}{}".format(path, name))
+        run("tar -xzf /tmp/{} -C {}{}/".format(file, path, name))
+        run("rm /tmp/{}".format(file))
+        run("mv {0}{1}/web_static/* {0}{1}/".format(path, name))
+        run("rm -rf {}{}/wen_static".format(path, name))
+        run("rm -rf /data/web_static/current")
+        run("ls -s {}{}/ /data/web_static/current".format(path, name))
 
-    if run("rm /tmp/{}".format(file)).failed:
+        return True
+    except:
         return False
-
-    if run("rm /data/web_static/current").failed:
-        return False
-
-    if run("ln -sf /data/web_static/releases/{} /data/web_static/current".format(name)).failed:
-        return False
-
-    return True
